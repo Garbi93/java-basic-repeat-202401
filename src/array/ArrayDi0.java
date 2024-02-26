74652d19a2610c45ee63ae4fc8a91a50300b3724
Garbi93 <iseung2093@gmail.com>
2024-02-26 13:46:26 +0900

    02/26 2차원 배열

---
+++ src/array/ArrayDi0.java
@@ -0,0 +1,27 @@
+package array;
+
+public class ArrayDi0 {
+    public static void main(String[] args) {
+        // 2x3 2차원 배열을 만든다.
+        int[][] arr = new int[2][3]; // 행2, 열3
+
+        arr[0][0] = 1; // 0, 0
+        arr[0][1] = 2; // 0, 1
+        arr[0][2] = 3; // 0, 2
+        arr[1][0] = 4; // 1, 0
+        arr[1][1] = 5; // 1, 1
+        arr[1][2] = 6; // 1, 2
+
+        // 0행 출력
+        System.out.print(arr[0][0] + " "); // 0열 출력
+        System.out.print(arr[0][1] + " "); // 1열 출력
+        System.out.print(arr[0][2] + " "); // 2열 출력
+        System.out.println(); // 한행 끝나면 라인 변경
+
+        // 1행 출력
+        System.out.print(arr[1][0] + " "); // 0열 출력
+        System.out.print(arr[1][1] + " "); // 1열 출력
+        System.out.print(arr[1][2] + " "); // 2열 출력
+        System.out.println(); // 한행 끝나면 라인 변경
+    }
+}
